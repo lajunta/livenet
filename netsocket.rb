a19929c709d429d85c16bf27bd95c2dabae76877
zxy <zxy@qq.com>
2014-09-12 02:00:14 +0800

    simple basic function

--- netsocket.rb
+++ netsocket.rb
@@ -1,10 +1,39 @@
 require "socket"
 
-server = TCPServer.new 12321
+if Gem::Platform.local.os == "darwin"
+  current_path = "/Users/zxy/current"
+  command_path = "/Users/zxy/command"
+else
+  current_path = "/root/current"
+  command_path = "/root"
+end
+
+def net_status
+  File.open "/Users/zxy/current" do |f|
+    return f.read
+  end
+end
+
+port = 12321
+server = TCPServer.new port
 
 loop do
   client = server.accept
-  client.puts "Hello!"
-  client.puts "Time is #{Time.now}"
+  command = client.gets.chomp.strip
+  client_ip = client.addr[3]
+  if client_ip =~ /192.168.\d{1,2}.100/ 
+    num = client_ip.split(".")[2]
+  else
+    num = 0
+  end
+  if command == "net"
+    fpath = command_path+"/net"+num.to_s
+    client.puts  "ok"
+  elsif command == "school"
+    fpath = command_path+"/school"
+    client.puts  "ok"
+  elsif command == "current"
+    client.puts net_status
+  end
   client.close
 end
