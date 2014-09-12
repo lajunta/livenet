571deb692f2780cb75cd5ce8025cf1343b7f3075
zxy <zxy@lwqzx.net>
2014-09-12 17:39:17 +0800

    second work

--- netsocket.rb
+++ netsocket.rb
@@ -1,39 +1,54 @@
 require "socket"
 
 if Gem::Platform.local.os == "darwin"
-  current_path = "/Users/zxy/current"
+  CurrentPath = "/Users/zxy/current"
   command_path = "/Users/zxy/command"
 else
-  current_path = "/root/current"
+  CurrentPath = "/root/current"
   command_path = "/root"
 end
 
 def net_status
-  File.open "/Users/zxy/current" do |f|
-    return f.read
+  if File.exist?(CurrentPath)
+    File.open CurrentPath do |f| 
+      return f.read
+    end
+  else
+    return "文件不存在"
   end
 end
 
 port = 12321
 server = TCPServer.new port
+client = server.accept
 
 loop do
-  client = server.accept
-  command = client.gets.chomp.strip
   client_ip = client.addr[3]
   if client_ip =~ /192.168.\d{1,2}.100/ 
     num = client_ip.split(".")[2]
   else
     num = 0
   end
+  command = client.gets.chomp.strip
   if command == "net"
-    fpath = command_path+"/net"+num.to_s
-    client.puts  "ok"
+    if net_status.include?("net")
+      fpath = command_path+"/allnet"
+    else
+      fpath = command_path+"/net"+num.to_s
+    end
+    %x(#{fpath}) if File.exist?(fpath)
+    client.puts $?==0 ? "ok" : "error"
   elsif command == "school"
     fpath = command_path+"/school"
-    client.puts  "ok"
+    %x(#{fpath}) if File.exist?(fpath)
+    client.puts $?==0 ? "ok" : "error"
   elsif command == "current"
     client.puts net_status
+  elsif command == "close"
+    puts "#{client_ip} closed"
+    client.close
+    client = server.accept
+  else
+    client.puts command
   end
-  client.close
 end
