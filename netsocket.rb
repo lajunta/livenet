77af11d02fa9f7b963e5f2f0bee797728e827f8a
zxy <zxy@lwqzx.net>
2014-09-14 18:21:40 +0800

    auto make config file

--- netsocket.rb
+++ netsocket.rb
@@ -15,43 +15,53 @@ def net_status
   end
 end
 
+def getneibor(num)
+  return num%2==0 ? num-1 : num+1
+end
+
 port = 12321
 server = TCPServer.new port
-client = server.accept
-
-loop do
-  client_ip = client.peeraddr[3]
-  if client_ip =~ /192.168.\d{1,2}.100/ 
-    num = client_ip.split(".")[2]
-  else
-    num = 0
-  end
-  command = client.gets.chomp.strip
-  if command == "net"
-    cstatus = net_status.chomp.strip
-    if cstatus.include?("net") and cstatus!="net"+num.to_s
-      fpath = command_path+"/allnet"
-    else
-      fpath = command_path+"/net"+num.to_s
+loop do 
+  Thread.start(server.accept) do |client|
+    loop do 
+      client_ip = client.peeraddr[3]
+      if client_ip =~ /192.168.\d{1,2}.100/ 
+        num = client_ip.split(".")[2]
+      else
+        num = 0
+      end
+      command = client.gets.chomp.strip
+      cstatus = net_status.chomp.strip
+      if command == "net"
+        if cstatus.include?("net") and cstatus!="net"+num.to_s
+          fpath = command_path+"/allnet"
+        else
+          fpath = command_path+"/net"+num.to_s
+        end
+        %x(#{fpath}) if File.exist?(fpath)
+        client.puts $?==0 ? "net_ok" : "net_error"
+        client.flush
+        puts command
+      elsif command == "school"
+        if cstatus=="school" or cstatus=="net"+num.to_s
+          fpath = command_path+"/school"
+          %x(#{fpath}) if File.exist?(fpath)
+	else
+	  $?=0
+        end
+        client.puts $?==0 ? "school_ok" : "school_error"
+        client.flush
+        puts command
+      elsif command == "current"
+        client.puts net_status.chomp.strip	
+        client.flush
+        puts "current status is "+net_status
+        puts command
+      elsif command == "close"
+        puts "#{client_ip} closed"
+        client.close
+        break
+      end
     end
-    %x(#{fpath}) if File.exist?(fpath)
-    client.puts $?==0 ? "net_ok" : "net_error"
-    client.flush
-    puts command
-  elsif command == "school"
-    fpath = command_path+"/school"
-    %x(#{fpath}) if File.exist?(fpath)
-    client.puts $?==0 ? "school_ok" : "school_error"
-    client.flush
-    puts command
-  elsif command == "current"
-    client.puts net_status.chomp.strip	
-    client.flush
-    puts "current status is "+net_status
-    puts command
-  elsif command == "close"
-    puts "#{client_ip} closed"
-    client.close
-    client = server.accept
   end
 end
