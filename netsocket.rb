4b20a31809ce04ec85769c8272ebdee27bece443
zxy <zxy@lwqzx.net>
2014-09-15 19:50:48 +0800

    enterInField
    modifiy enterInField

--- netsocket.rb
+++ netsocket.rb
@@ -15,25 +15,22 @@ def net_status
   end
 end
 
-def getneibor(num)
-  return num%2==0 ? num-1 : num+1
-end
-
 port = 12321
 server = TCPServer.new port
+
 loop do 
   Thread.start(server.accept) do |client|
     loop do 
       client_ip = client.peeraddr[3]
       if client_ip =~ /192.168.\d{1,2}.100/ 
-        num = client_ip.split(".")[2]
+        num = client_ip.split(".")[2].to_i
       else
         num = 0
       end
       command = client.gets.chomp.strip
       cstatus = net_status.chomp.strip
       if command == "net"
-        if cstatus.include?("net") and cstatus!="net"+num.to_s
+        if cstatus.include?("net") 
           fpath = command_path+"/allnet"
         else
           fpath = command_path+"/net"+num.to_s
@@ -41,22 +38,20 @@ loop do
         %x(#{fpath}) if File.exist?(fpath)
         client.puts $?==0 ? "net_ok" : "net_error"
         client.flush
-        puts command
       elsif command == "school"
-        if cstatus=="school" or cstatus=="net"+num.to_s
+        neibor_num = num%2==0 ? num-1 : num+1
+        if cstatus.include?("net") and cstatus!="net"+num.to_s
+          fpath = command_path+"/net"+neibor_num.to_s
+        else
           fpath = command_path+"/school"
-          %x(#{fpath}) if File.exist?(fpath)
-	else
-	  $?=0
         end
+        %x(#{fpath}) if File.exist?(fpath)
         client.puts $?==0 ? "school_ok" : "school_error"
         client.flush
-        puts command
       elsif command == "current"
         client.puts net_status.chomp.strip	
         client.flush
         puts "current status is "+net_status
-        puts command
       elsif command == "close"
         puts "#{client_ip} closed"
         client.close
